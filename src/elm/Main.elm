64987652c9d39b9866079571f823508a7834832e
Andrew Thompson <andrew.thompson@qmul.ac.uk>
2020-03-17 08:13:41 +0000

    :sparkles: Check outDir and prompt user if we need to overwrite or create that directory.

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -4,10 +4,13 @@ port module Main exposing (main)
 import Arguments exposing (Arguments)
 import Json.Decode
 
+import Ports.Filesystem as Filesystem
+import Ports.Prompt as Prompt
+
 
 {- Ports -------------------------------------------------------------------- -}
 {-| -}
-port log : String -> Cmd msg
+port exit : Int -> Cmd msg
 
 
 {- Main --------------------------------------------------------------------- -}
@@ -28,7 +31,7 @@ main : Program Flags (Maybe Model) Msg
 main =
   Platform.worker
     { init = init
-    , update = \msg model -> 
+    , update = \msg model ->
         Maybe.map (update msg) model
           |> Maybe.andThen (Just << Tuple.mapFirst Just)
           |> Maybe.withDefault (Nothing, Cmd.none)
@@ -69,7 +72,7 @@ init ({ args } as flags) =
   if String.startsWith "--help" args then
     initHelp args
 
-  else 
+  else
     initApp flags
 
 {-| -}
@@ -78,7 +81,8 @@ initHelp args =
   ( Nothing
   , Arguments.parseHelp args
       |> Arguments.showHelp
-      |> log
+      |> Prompt.notify
+      |> Prompt.runAction never
   )
 
 {-| Even though we're initialising the "real" app at this point, now we have a
@@ -98,7 +102,7 @@ initApp { args, packages } =
           , overwriteFiles = False
           , packages = decodePackageNames packages
           }
-      , Cmd.none
+      , checkOutDir arguments.dir
       )
 
     Err _ ->
@@ -109,23 +113,27 @@ initApp { args, packages } =
 {-| -}
 decodePackageNames : Json.Decode.Value -> List String
 decodePackageNames packages =
-  packages 
-    |> Json.Decode.decodeValue (Json.Decode.list <| Json.Decode.field "name" Json.Decode.string)
+  packages
+    |> Json.Decode.decodeValue
+      (Json.Decode.list <| Json.Decode.field "name" Json.Decode.string)
     |> Result.withDefault []
+
 
 {- Update ------------------------------------------------------------------- -}
 {-| -}
 type Msg
-  = None
+  = GotFilesystemResponse (Filesystem.Response FilesystemTag)
+  | GotPromptResponse (Prompt.Response PromptTag)
 
 {-| -}
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
-    None ->
-      ( model
-      , Cmd.none
-      )
+    GotPromptResponse response ->
+      gotPromptResponse response model
+
+    GotFilesystemResponse response ->
+      gotFilesystemResponse response model
 
 
 {- Subscriptions ------------------------------------------------------------ -}
@@ -133,5 +141,162 @@ update msg model =
 subscriptions : Model -> Sub Msg
 subscriptions _ =
   Sub.batch
-    [
+    [ Prompt.onResponse promptTagFromString GotPromptResponse
+    , Filesystem.onResponse filesystemTagFromString GotFilesystemResponse
     ]
+
+
+{- Ports: Filesystem -------------------------------------------------------- -}
+{-| The system we have in place for using ports allows us to attach tags to port
+calls we expect to return something. We can use whatever we want for the tag
+as long as we supply toString and fromString functions.
+
+  • OutDir
+    We use this tag to get a response from read the contents of the output
+    directory.
+
+  • UnknownFilesystemTag
+    For now, the fromString function doesn't return a `Maybe`, so we need to
+    have a variant we can return if we get an unknown (or empty) string as the
+    tag from javascript.
+-}
+type FilesystemTag
+  = OutDir
+  | UnknownFilesystemTag
+
+checkOutDir : String -> Cmd msg
+checkOutDir outDir =
+  Filesystem.sequence filesystemTagToString
+    |> Filesystem.andThen (Filesystem.readDir outDir)
+    |> Filesystem.andThen (Filesystem.expectDir OutDir)
+    |> Filesystem.run
+
+{-| -}
+gotFilesystemResponse : Filesystem.Response FilesystemTag -> Model -> (Model, Cmd Msg)
+gotFilesystemResponse response model =
+  case response of
+    -- If there are files in the directory we should prompt the user to confirm
+    -- if they're happy to proceed. We don't actually care what the files are
+    -- (maybe we will in the future).
+    Filesystem.GotDir OutDir (_ :: _) ->
+      ( model
+      , askToOverwrite
+      )
+
+    -- If the directory doesn't exist we should prompt the user to ask if they
+    -- want us to create it for them.
+    Filesystem.Error OutDir "ENOENT" ->
+      ( model
+      , askToCreateDirectory
+      )
+
+    _ ->
+      ( model
+      , Cmd.none
+      )
+
+filesystemTagToString : FilesystemTag -> String
+filesystemTagToString tag =
+  case tag of
+    OutDir ->
+      "OutDir"
+
+    UnknownFilesystemTag ->
+      ""
+
+filesystemTagFromString : String -> FilesystemTag
+filesystemTagFromString tag =
+  case tag of
+    "OutDir" ->
+      OutDir
+
+    _ ->
+      UnknownFilesystemTag
+
+
+{- Ports: Prompt ------------------------------------------------------------ -}
+{-| -}
+type PromptTag
+  = CreateDirectory
+  | OverwriteFiles
+  | UnknownPromptTag
+
+askToCreateDirectory : Cmd msg
+askToCreateDirectory =
+  Prompt.sequence promptTagToString
+    |> Prompt.andThen (Prompt.askYesNo "Looks like the directory doesn't exist, should we create it?")
+    |> Prompt.andThen (Prompt.expectBool CreateDirectory)
+    |> Prompt.run
+
+{-| -}
+askToOverwrite : Cmd msg
+askToOverwrite =
+  Prompt.sequence promptTagToString
+    |> Prompt.andThen (Prompt.askYesNo "Looks like the directory isn't empty, should we conitnue anyway?")
+    |> Prompt.andThen (Prompt.expectBool OverwriteFiles)
+    |> Prompt.run
+
+{-| -}
+gotPromptResponse : Prompt.Response PromptTag -> Model -> (Model, Cmd Msg)
+gotPromptResponse response model =
+  case response of
+    -- The user has said that it's OK to create the directory when it doesn't
+    -- exist. If the user supplied an outDir that was a path with multiple
+    -- directories that don't exist, we'll create them all.
+    Prompt.GotBool CreateDirectory True ->
+      ( { model
+        | createDirectory = True
+        }
+      , Cmd.none
+      )
+
+    -- The user has said it's *not* OK to create the output directory if
+    -- necessary. This means we need to prompt rhem for a new outDir.
+    Prompt.GotBool CreateDirectory False ->
+      ( model
+      , Debug.todo "Prompt user for a new output directory."
+      )
+
+    -- 
+    Prompt.GotBool OverwriteFiles True ->
+      ( { model
+        | overwriteFiles = True
+        }
+      , Cmd.none
+      )
+
+    Prompt.GotBool OverwriteFiles False ->
+      ( model
+      , Debug.todo "Prompt user for a new output directory."
+      )
+
+    _ ->
+      ( model
+      , Cmd.none
+      )
+
+{-| -}
+promptTagToString : PromptTag -> String
+promptTagToString tag =
+  case tag of
+    CreateDirectory ->
+      "CreateDirectory"
+
+    OverwriteFiles ->
+      "OverwriteFiles"
+
+    UnknownPromptTag ->
+      ""
+
+{-| -}
+promptTagFromString : String -> PromptTag
+promptTagFromString tag =
+  case tag of
+    "CreateDirectory" ->
+      CreateDirectory
+
+    "OverwriteFiles" ->
+      OverwriteFiles
+
+    _ ->
+      UnknownPromptTag
