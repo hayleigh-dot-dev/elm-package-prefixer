6e3ba5b138c6bd20b72452323a3e0011159af178
Andrew Thompson <andrew.thompson@qmul.ac.uk>
2020-03-17 08:53:06 +0000

    :wrench: Add comment to generated Env.elm file to remind people not to edit it.

--- build.js
+++ build.js
@@ -1,7 +1,41 @@
 const Bundler = require('parcel-bundler')
+const Filesystem = require('fs')
 const Path = require('path')
 
-const entry = Path.join(__dirname, 'src', './elm-package-prefixer.js')
+function generateEnvModule () {
+  const envRegex = /(^NODE_ENV$)|(^ELM_*.*)/
+  const envVars = Object.entries(process.env)
+    .filter(([k]) => k.match(envRegex))
+    .map(([k, v]) => [k.toLocaleLowerCase(), v])
+
+  const header = [
+    `module Env exposing (${envVars.join(', ')})`,
+    '',
+    '-- This file was generated automatically at build time. Do not edit it and',
+    '-- do not check it into git.',
+    ''
+  ].join('\n')
+
+  const module = envVars.reduce(
+    (m, [k, v]) => {
+      return [
+        m,
+        '',
+        `${k} : String`,
+        `${k} =`,
+        `  "${v}"`
+      ].join('\n')
+    },
+    header
+  )
+
+  return Filesystem.promises.writeFile(
+    Path.join(__dirname, 'src', 'elm', 'Env.elm'),
+    module
+  )
+}
+
+const entry = Path.join(__dirname, 'src', 'elm-package-prefixer.js')
 const options = {
   outDir: './bin',
   outFile: process.env.NODE_ENV === 'production' ? 'elm-package-prefixer.js' : 'elm-package-prefixer.dev.js',
@@ -19,4 +53,6 @@ const options = {
   autoInstall: true
 }
 
-new Bundler(entry, options).bundle()
+generateEnvModule().then(() => {
+  new Bundler(entry, options).bundle()
+})
