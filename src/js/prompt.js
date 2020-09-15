461577e5b19925f10cc0e94e1254cb9d19f4e5b4
Andrew Thompson <andrew.thompson@qmul.ac.uk>
2020-09-15 19:52:01 +0100

    :art: Let standardjs mangle our code.

--- src/js/prompt.js
+++ src/js/prompt.js
@@ -1,95 +1,109 @@
-const Readline = require('readline')
+const Readline = require("readline");
 const Prompt = Readline.createInterface({
   input: process.stdin,
-  output: process.stdout
-})
+  output: process.stdout,
+});
 
 /* */
-export function init (send, recv) {
-  recv(sequence => {
-    const responses = []
+export function init(send, recv) {
+  recv((sequence) => {
+    const responses = [];
 
-    sequence.forEach(msg => {
+    sequence.forEach((msg) => {
       switch (msg.$) {
-        case 'Expect':
-          expect(msg, responses, send)
-          break
+        case "Expect":
+          expect(msg, responses, send);
+          break;
 
-        case 'Ask':
-          ask(msg, responses)
-          break
+        case "Ask":
+          ask(msg, responses);
+          break;
 
-        case 'AskYesNo':
-          askYesNo(msg, responses)
-          break
+        case "AskYesNo":
+          askYesNo(msg, responses);
+          break;
 
-        case 'Notify':
-          notify(msg)
-          break
+        case "Notify":
+          notify(msg);
+          break;
 
         default:
-          console.warn(`Unknown msg: ${msg.$}`)
-          break
+          console.warn(`Unknown msg: ${msg.$}`);
+          break;
       }
-    })
-  })
+    });
+  });
 }
 
 /* */
-function expect ({ tag, type }, data, send) {
+function expect({ tag, type }, data, send) {
   // This mutates the `data` array
-  const pendingReponse = data.shift()
+  const pendingResponse = data.shift();
 
-  if (pendingReponse) {
-    pendingReponse
-      .then(res => {
+  if (pendingResponse) {
+    pendingResponse
+      .then((res) => {
         res.type === type
           ? send({ tag, ...res })
-          : send({ tag, $: 'Err', message: `Expecting response of type ${type} but got ${res.type} instead.` })
+          : send({
+              tag,
+              $: "Err",
+              message: `Expecting response of type ${type} but got ${res.type} instead.`,
+            });
       })
-      .catch(err => {
-        send({ tag, $: 'Err', message: err.code })
-      })
+      .catch((err) => {
+        send({ tag, $: "Err", message: err.code });
+      });
   }
 }
 
 /* */
-function ask ({ question }, data) {
+function ask({ question }, data) {
   data.push(
-    new Promise(resolve => {
-      Prompt.question(question + '\n', answer => {
+    new Promise((resolve) => {
+      Prompt.question(question + "\n", (answer) => {
         resolve({
-          $: 'GotString', type: 'String', response: answer
-        })
-      })
+          $: "GotString",
+          type: "String",
+          response: answer,
+        });
+      });
     })
-  )
+  );
 }
 
 /* */
-function askYesNo ({ question }, data) {
+function askYesNo({ question }, data) {
   const validReplies = [
-    'Yes', 'yes', 'Ye', 'ye', 'Y', 'y',
-    'No', 'no', 'N', 'n'
-  ]
+    "Yes",
+    "yes",
+    "Ye",
+    "ye",
+    "Y",
+    "y",
+    "No",
+    "no",
+    "N",
+    "n",
+  ];
 
   data.push(
     new Promise((resolve, reject) => {
-      Prompt.question(question + ' (Y/N): ', answer => {
-        if (validReplies.some(r => answer === r.trim())) {
+      Prompt.question(question + " (Y/N): ", (answer) => {
+        if (validReplies.some((r) => answer === r.trim())) {
           resolve({
-            $: 'GotBool', type: 'Bool', response: answer
-          })
+            $: "GotBool",
+            type: "Bool",
+            response: answer,
+          });
         } else {
-          reject(
-            new Error()
-          )
+          reject(new Error());
         }
-      })
+      });
     })
-  )
+  );
 }
 
-function notify ({ notice }) {
-  console.log(notice)
+function notify({ notice }) {
+  console.log(notice);
 }
